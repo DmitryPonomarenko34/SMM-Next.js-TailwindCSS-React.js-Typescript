4cd75f2d467d26780db58e4f960e25e99b61bad2
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-02-20 18:10:52 +0200

    change theme variable to resolvedTheme

--- app/ThemeBtn.tsx
+++ app/ThemeBtn.tsx
@@ -5,24 +5,25 @@ import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 export default function ThemeBtn() {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme, resolvedTheme, systemTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
-    if (systemTheme && resolvedTheme) {
-      setTheme(resolvedTheme);
-    }
   }, []);
 
   if (!mounted) {
     return null;
   }
 
-  const forceTheme = theme === "dark" ? "light" : "dark";
+  const forceTheme = resolvedTheme === "dark" ? "light" : "dark";
 
   return (
     <button className="w-6 h-6" onClick={() => setTheme(forceTheme)}>
-      {theme === "dark" ? <SunIcon title="light" /> : <MoonIcon title="dark" />}
+      {resolvedTheme === "dark" ? (
+        <SunIcon title="light" />
+      ) : (
+        <MoonIcon title="dark" />
+      )}
     </button>
   );
 }
