0561319cc6bd03ac12d5e2be509b572d8d3d18eb
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-02-24 15:34:04 +0200

    replace Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import FirtsSection from "./FirstSection";
 
 export default function HomePage() {
   return (
     <>
       <FirtsSection />
+      <Link href="/">Services</Link>
+      <Link href="/services">Services</Link>
     </>
   );
 }
