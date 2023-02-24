379f2c4254a3cdee78ef91b5be332804b9166595
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-02-24 15:24:02 +0200

    change Head component

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
       <body className={nunitoSans.className}>
         <Providers>
           <Header />
